e1c4410404f85e2a3d9c14112239f0efa3d9a214
davephoenix360 <davediepreye05@gmail.com>
2024-05-25 14:26:25 -0600

    Just added a few things. Need to complete the "how to play func". The placement is off and I need to handle when the text is too long for the page. i.e. create a scroll feature.

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 
 def start_screen(screen, screen_dim, font):
-    items = draw_start_screen(screen, screen_dim, font)
+    items = draw_start_screen(screen, screen_dim, font) # These are itmes that need to be redrawn
     enter_pressed = False
     
     while not enter_pressed:
@@ -21,7 +21,7 @@ def start_screen(screen, screen_dim, font):
       
 def draw_start_screen(screen, screen_dim, font):
     screen.fill((0, 117, 191))
-    text1 = font[0].render("MATH QUIZ TRAINING APP", True, (255, 255, 255))
+    text1 = font[0].render("STEM DUEL PREP", True, (255, 255, 255))
     text_rect1 = text1.get_rect()
     text_rect1.center = ((screen_dim[0] / 2), (screen_dim[1] / 2 - 100))
     screen.blit(text1, text_rect1)
@@ -45,7 +45,7 @@ def redraw_start_window(screen, items):
 def menu_screen(screen, screen_dim, font):
     enter_pressed = False
     selected = 1
-    options = ["START", "INSTRUCTIONS", "QUIT"]
+    options = ["PRIVATE GAME", "HOST A QUIZ SHOW", "JOIN A QUIZ SHOW", "HOW TO PLAY", "QUIT"]
     
     while not enter_pressed:
         for event in pygame.event.get():
@@ -59,7 +59,7 @@ def menu_screen(screen, screen_dim, font):
                     selected = max(1, selected)
                 elif event.key == pygame.K_DOWN:
                     selected += 1
-                    selected = min(3, selected)
+                    selected = min(len(options), selected)
         
         items = draw_menu_screen(screen, screen_dim, font, options, selected)
         redraw_menu_window(screen, items)
@@ -72,7 +72,7 @@ def draw_menu_screen(screen, screen_dim, font, options, selected = 1):
     back_color = (0, 117, 191)
     
     screen.fill(back_color)
-    title_text = font[0].render("MATH QUIZ TRAINING APP", True, (255, 255, 255))
+    title_text = font[0].render("STEM DUEL RPEP", True, (255, 255, 255))
     title_text_rect = title_text.get_rect()
     title_text_rect.center = ((screen_dim[0] / 2), (screen_dim[1] / 2 - 250))
     screen.blit(title_text, title_text_rect)
@@ -124,28 +124,85 @@ def redraw_menu_window(screen, items):
     
     pygame.display.update()
 
+def private_game(screen, screen_dim, fonts):
+    pass
+
+def host_game(screen, screen_dim, fonts):
+    pass
+
+def join_game(screen, screen_dim, fonts):
+    pass
+
+def how_to_play(screen, screen_dim, fonts):
+    with open("instructions.txt", "r") as f:
+        lines = f.readlines()
+        instructions_text = []
+        header = None
+        for i, line in enumerate(lines):
+            if line.startswith("HDR"):
+                header_txt = fonts[1].render(line.split(":")[1].strip(), True, (255, 255, 255))
+                header_rect = header_txt.get_rect()
+                header_rect.center = (screen_dim[0] / 2, screen_dim[1] / 2 + (25 * i))
+                instructions_text.append((header_txt, header_rect))
+                continue
+            
+            instruc_txt = (fonts[2].render(line, True, (255, 255, 255)))
+            instruc_txt_rect = instruc_txt.get_rect()
+            instruc_txt_rect.center = (screen_dim[0] / 2, screen_dim[1] / 2 + (25 * i))
+            instructions_text.append((instruc_txt, instruc_txt_rect))
+
+    esc_pressed = False
+    while not esc_pressed:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    esc_pressed = True
+            
+        redraw_instruction_screen(screen, instructions_text)
+    
+    
+            
+def redraw_instruction_screen(screen, items):
+    screen.fill((0, 117, 191))
+    for item in items:
+        screen.blit(item[0], item[1])
+    
+    pygame.display.update()
+    
+
 def main_interface(SCREEN_WIDTH, SCREEN_HEIGHT):
     pygame.init()
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     screen_dim = screen.get_size()
-    pygame.display.set_caption("MATH QUIZ TRAINING APP")
+    pygame.display.set_caption("STEM DUEL RPEP")
+    
+    # The different fonts that will be used in the app
     font = pygame.font.SysFont('comicsans', 40)
     font1 = pygame.font.SysFont('comicsans', 30)
     font2 = pygame.font.SysFont('comicsans', 20)
     font3 = pygame.font.SysFont('comicsans', 15)
+    
+    # A list of fonts that will be used in the app
     fonts = (font, font1, font2, font3)
 
     start_screen(screen, screen_dim, fonts) 
     selected, options = menu_screen(screen, screen_dim, fonts)
-    # selected can be 1, 2, 3, options is "START", "INSTRUCTIONS", "QUIT"
+    # selected can be 1, 2, 3, 4, 5 options is "PRIVATE GAME", "HOST A QUIZ SHOW", "JOIN A QUIZ SHOW", "HOW TO PLAY", "QUIT"
     
     if selected == 1:
-        pass
+        private_game(screen, screen_dim, fonts)
     elif selected == 2:
-        pass
+        host_game(screen, screen_dim, fonts)
     elif selected == 3:
-        sys.exit()  
+        join_game(screen, screen_dim, fonts)
+    elif selected == 4:
+        how_to_play(screen, screen_dim, fonts)
+        main_interface(SCREEN_WIDTH, SCREEN_HEIGHT)
+    elif selected == 5:
+        sys.exit()
 
 if __name__ == "__main__":
     main_interface(SCREEN_HEIGHT=SCREEN_HEIGHT, SCREEN_WIDTH=SCREEN_WIDTH)
